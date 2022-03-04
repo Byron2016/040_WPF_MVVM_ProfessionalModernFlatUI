46cb52ba3ba118e6503bcc304178cca679a594cd
Byron 2016 <bgva2005@yahoo.com>
2022-03-03 20:09:09 -0500

    feat: create mvvm logic

---
+++ ModernDesign/Core/ObservableObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ModernDesign.Core
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void onPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
